f21361cfe13e6e3d3ad77ee3e58b20f8f0cc354d
Jakub Wilk <jwilk@jwilk.net>
2017-06-14 14:17:30 +0200

    Initial import.

---
+++ cowproxy.py
@@ -0,0 +1,26 @@
+# encoding=UTF-8
+
+# Copyright © 2017 Jakub Wilk <jwilk@jwilk.net>
+# SPDX-License-Identifier: MIT
+
+try:
+    from mitmproxy.http import HTTPResponse  # mitmproxy >= 1.0
+except ImportError:
+    from mitmproxy.models import HTTPResponse
+
+cow = r'''
+^__^
+(oo)\_______
+(__)\       )\/\
+    ||----w |
+    ||     ||
+'''.lstrip('\n')
+
+def request(flow):
+    flow.response = HTTPResponse.make(
+        200,
+        cow,
+        {"Content-Type": 'text/plain'}
+    )
+
+# vim:ts=4 sts=4 sw=4 et
